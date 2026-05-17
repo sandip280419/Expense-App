745f3ed7fb433e18692ebec6a31b5f8474f2286a
sandip280419 <sandipkarmakary1@gmail.com>
2026-05-17 18:33:32 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,56 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ReactDOM from 'react-dom/client'
 
 function App() {
+  const [text, setText] = useState("")
+  const [amount, setAmount] = useState("")
+  const [items, setItems] = useState([])
+
+  const addExpense = () => {
+    if (!text || !amount) return
+
+    const newItem = {
+      text,
+      amount
+    }
+
+    setItems([...items, newItem])
+
+    setText("")
+    setAmount("")
+  }
+
   return (
-    <div style={{padding: 20}}>
-      <h1>Expense App</h1>
-      <p>App Running Successfully ✅</p>
+    <div style={{padding:20,fontFamily:"Arial"}}>
+      <h1>বাংলা Expense App 💰</h1>
+
+      <input
+        placeholder="খরচের নাম"
+        value={text}
+        onChange={(e)=>setText(e.target.value)}
+      />
+
+      <br /><br />
+
+      <input
+        placeholder="টাকার পরিমাণ"
+        value={amount}
+        onChange={(e)=>setAmount(e.target.value)}
+      />
+
+      <br /><br />
+
+      <button onClick={addExpense}>
+        Add Expense
+      </button>
+
+      <hr />
+
+      {items.map((item,index)=>(
+        <div key={index}>
+          {item.text} - ₹{item.amount}
+        </div>
+      ))}
     </div>
   )
 }
